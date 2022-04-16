03598d9bb09c649a2e0bf3cd43fb61a528b64a37
rogerbenedik <rogerbenedik@hotmail.com>
2022-04-15 20:48:07 -0400

    Cleaning App, Creating Logo component basic structure

---
+++ src/components/Logo/Logo.styles.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const LogoStyles = styled.div`
+    background: #2F2F2F;
+    padding: 20px;
+    display: flex;
+    max-width: 200px;
+
+    .box {
+        color: #fff;
+    }
+
+    .white-box {
+        background: #fff;
+        color: #2F2F2F;
+    }
+`;
+
+export default LogoStyles;
---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import LogoStyles from "./Logo.styles";
+
+const Logo = () => {
+  return (
+    <LogoStyles>
+      <div className='box'>Rock' N </div>
+      <div className='box white-box'>Code</div>
+    </LogoStyles>
+  );
+};
+
+export default Logo;
