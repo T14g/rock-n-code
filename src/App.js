ec9f42ac4a2f053310f14af61cfa35696e7510ca
Tiago <ottiagos@gmail.com>
2022-04-17 23:24:40 -0300

    Header simple component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Logo from "./components/Logo";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
-      <Logo />
+      <Header />
     </div>
   );
 };
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Logo from '../Logo';
+
+const Header = () => {
+    return(
+        <div>
+            <Logo />
+            <ul>
+                <li>Item</li>
+                <li>Item</li>
+                <li>Item</li>
+            </ul>
+        </div>
+    )
+};
+
+export default Header;
