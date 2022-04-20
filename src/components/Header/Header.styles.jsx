bd8d58c3e5a4d9ee7aaddda17516acf8386acf22
Tiago <ottiagos@gmail.com>
2022-04-19 23:49:50 -0300

    Header styling

--- src/components/Header/Header.styles.jsx
+++ src/components/Header/Header.styles.jsx
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 
 const HeaderStyles = styled.div`
+    display: flex;
 
+    ul {
+        li {
+            display: inline;
+            margin-right: 10px;
+        }
+    }
 `;
 
 export default HeaderStyles
