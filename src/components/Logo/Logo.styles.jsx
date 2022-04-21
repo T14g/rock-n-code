5e5005191444bf0a9b2bfc15b8c5b311fc742edb
Tiago <ottiagos@gmail.com>
2022-04-20 23:51:39 -0300

    Logo styled

--- src/components/Logo/Logo.styles.jsx
+++ src/components/Logo/Logo.styles.jsx
@@ -2,17 +2,24 @@ import styled from 'styled-components';
 
 const LogoStyles = styled.div`
     background: #2F2F2F;
-    padding: 20px;
+    padding: 10px 14px;
     display: flex;
     max-width: 200px;
+    font-size: 20px;
+    font-weight: 500;
+    text-transform: uppercase;
 
     .box {
         color: #fff;
+        padding: 5px;
     }
 
     .white-box {
         background: #fff;
         color: #2F2F2F;
+        margin-left: 5px;
+        padding: 4px;
+        font-weight: 800;
     }
 `;
 
